936a91eeae54bb1041c4f42920c63acceb2762b6
StefanDoychinovDoynov <whycallsteven@gmail.com>
2024-11-10 18:20:24 +0100

    Starting css

--- client/src/components/WelcomePage.jsx
+++ client/src/components/WelcomePage.jsx
@@ -3,9 +3,9 @@ import { MenuSections } from './Page'
 
 const WelcomePage = () => {
     return (
-        <div>
+        <div className='Everything'>
             <MenuSections />
-
+            <p id='thisthing'>RANDOMMM</p>
             {/* Tova sa butoni za productite i vaucherite. Ne znam dali trqbva da gi ima tuk */}
             <button onClick={() => { document.location.href = "/products" }}>Products</button>
             <button onClick={() => { document.location.href = "/vouchers" }}>Vouchers</button>

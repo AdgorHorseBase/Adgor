a830175effa6eaa59d9cb45392e3304634f24a44
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-11-12 21:50:58 +0200

    Changed the menu; Added test pages

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,16 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 import axios from 'axios';
 import "./page.css"
 const URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:8080";
 
 const MenuSections = () => {
-  const [structure, setStruct] = useState(false)
+  const [structure, setStruct] = useState(false);
+  const [lang, setLang] = useState('bg');
+  const titlesFetched = useRef(false);
 
   useEffect(() => {
     const getStruct = async () => {
       try {
         const schema = await axios.get(`/server/files/structure.json`);
+        console.log(schema.data);
         setStruct(schema.data);
       } catch(err) {
         console.log("Error:", err);
@@ -20,30 +23,71 @@ const MenuSections = () => {
     getStruct();
   }, [])
 
+  useEffect(() => {
+    const fetchTitles = async (structure) => {
+      const updatedStructure = { ...structure };
+
+      for (const dir in updatedStructure) {
+        if (updatedStructure[dir].type === 'directory') {
+          for (const page of updatedStructure[dir].contents) {
+            try {
+              const titleResponse = await axios.get(`/server/uploads${dir}/${page}/schema.json`);
+              updatedStructure[dir].contents = updatedStructure[dir].contents.map(p => 
+                p === page ? { page, titleBg: titleResponse.data.titleBg, titleEn: titleResponse.data.titleEn } : p
+              );
+            } catch (err) {
+              console.log(`Error fetching title for ${dir}/${page}:`, err);
+            }
+          }
+        } else if (updatedStructure[dir].type === 'file') {
+          try {
+            const titleResponse = await axios.get(`/server/uploads${dir}/schema.json`);
+            updatedStructure[dir] = { ...updatedStructure[dir], titleBg: titleResponse.data.titleBg, titleEn: titleResponse.data.titleEn };
+          } catch (err) {
+            console.log(`Error fetching title for ${dir}:`, err);
+          }
+        }
+      }
+
+      setStruct(updatedStructure);
+    };
+
+    if (structure && !titlesFetched.current) {
+      fetchTitles(structure);
+      titlesFetched.current = true;
+    }
+  }, [structure]);
+
+  useEffect(() => {
+    if (localStorage.getItem('lang')) {
+      setLang(localStorage.getItem('lang'));
+    }
+  }, [localStorage.getItem('lang')]);
+
   return (
-    <>
-      <button onClick={() => { document.location.href = "/" }}>Home</button>
+    <div className='Menu'>
+      <button onClick={() => { document.location.href = "/" }}>{lang === 'bg' ? 'Начало' : 'Home'}</button>
       {Object.keys(structure).map((dir) => (
         structure[dir].type === 'directory' ? structure[dir].contents && (
           <select
             key={dir} // Ensures each <select> element has a unique key
             onChange={(e) => { window.location.href = e.target.value; }}
           >
-            <option value="">{dir.slice(1)}</option>
-            {structure[dir].contents.map((page, index) => (
-              <option key={`${dir}-${page}-${index}`} value={`/page${dir}/${page}`}>
-                {page}
-              </option>
-            ))}
+          <option value="">{lang === 'bg' ? structure[dir].titleBg : structure[dir].titleEn}</option>
+          {structure[dir].contents.map((page, index) => (
+            <option key={`${dir}-${page}-${index}`} value={`/page${dir}/${page.page}`}>
+              {lang === 'bg' ? page.titleBg : page.titleEn}
+            </option>
+        ))}
           </select>
         ) :
         structure[dir].type === 'file' && (
           <button key={dir} onClick={() => { document.location.href = `/page${dir}` }}>
-            {dir.slice(1)}
+            {lang === 'bg' ? structure[dir].titleBg : structure[dir].titleEn}
           </button>
         )
       ))}
-    </>
+    </div>
   )
 }
 
@@ -88,8 +132,6 @@ function Page() {
   useEffect(() => {
     const elementsBg = document.querySelectorAll('.bg');
     const elementsEn = document.querySelectorAll('.en');
-
-    console.log(elementsBg, elementsEn);
 
     if (lang === 'en') {
       elementsBg.forEach(el => el.style.display = 'none');

dfc6ef485cd3d406dcb409ac930b46f10e1d69f6
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2025-01-06 16:11:31 +0200

    🔝 Add ReturnToTop component for improved user navigation

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Vouchers from './components/Vouchers.jsx';
 import WelcomePage from './components/WelcomePage.jsx';
 import Footer from './components/Footer.jsx';
 import FooterMobile from './components/FooterMobile.jsx';
+import { useEffect, useState } from 'react';
 
 const isAuthenticated = () => {
   return true;
@@ -14,6 +15,54 @@ const isAuthenticated = () => {
 
 const PrivateRoute = ({ children }) => {
   return isAuthenticated() ? children : <Navigate to="/" />;
+};
+
+const ReturnToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const returnToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 150) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', toggleVisibility);
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
+  const styles = {
+    position: 'fixed',
+    bottom: '20px',
+    right: '20px',
+    backgroundColor: 'rgb(51, 28, 20)',
+    color: '#fff',
+    borderRadius: '50%',
+    padding: '10px',
+    cursor: 'pointer',
+    zIndex: 1000,
+  };
+
+  return (
+    isVisible && (
+      <div className="return-to-top" onClick={returnToTop} style={styles}>
+        <svg fill="#fff" height="28px" width="32px" viewBox="0 0 330 330"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path id="XMLID_224_" d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394 l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393 C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"></path> </g></svg>
+      </div>
+    )
+  );
+
 };
 
 function App() {
@@ -43,6 +92,8 @@ function App() {
           } 
         />
       </Routes>
+
+      <ReturnToTop />
 
       {showFooter && (window.innerWidth > 750 ? <Footer /> : <FooterMobile />)}
     </BrowserRouter>

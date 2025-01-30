002fae9e3bf1db031b2a12edd961aecf5759580f
VencislavManoilov <vencislav2.manoilov@gmail.com>
2025-01-30 11:55:04 +0200

    🍫 Fixed the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -58,28 +58,33 @@ const Footer = () => {
 
       <div className="footer-container">
         <h1>
-          Looking for an <br></br>
+          Looking for an <br />
           <span className="adventure-container">
             <span className="adventure">adventure?</span>
           </span>
         </h1>
-        <div className="contact-form">
+
+        <div className="footerContacts">
           <h1>Contact Info</h1>
-          <p>
-            Horse riding <br></br>"Adgor"
-          </p>
-          <p className="view-map">View map</p>
-          <p className="working-hours-text">Working hours:</p>
-          <p className="working-days">Wed-Sun:</p>
-          <p className="working-hours">10:00-18:00</p>
-        </div>
-        <div className="contact-form2">
-          <h1>Phone:</h1>
-          <p>0887 467 527</p>
-          <p>0888 877 056</p>
-          <p>0888 014 425</p>
-          <p className="email-text">Email:</p>
-          <p className="email">adgor@abv.bg</p>
+          <div className="contacts">
+            <div className="contact-form">
+              <p className="darker">
+                Horse riding <br></br>"Adgor"
+              </p>
+              <p className="view-map">View map</p>
+              <p className="darker">Working hours:</p>
+              <p className="working-days">Wed-Sun:</p>
+              <p className="working-hours">10:00-18:00</p>
+            </div>
+            <div className="contact-form2">
+              <p className="darker">Phone:</p>
+              <p>0887 467 527</p>
+              <p>0888 877 056</p>
+              <p>0888 014 425</p>
+              <p className="darker">Email:</p>
+              <p className="email">adgor@abv.bg</p>
+            </div>
+          </div>
         </div>
       </div>
       <div className="footer2-container">
